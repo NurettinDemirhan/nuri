41e388d0d3b0eb5cb02b3dfcf0d59733793b043b
Nurettin Demirhan <ndemirhan1907@gmail.com>
2023-06-21 00:41:40 +0300

    added contact component. changed the css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import './App.css';
 import "./Navbar.css";
 import About from './components/about/About';
+import Contact from './components/contact/Contact';
 
+import { Link, animateScroll as scroll } from "react-scroll";
 
+scroll.scrollToTop();
 
 function App() {
+
   return (
+    
+
     <div className="app">
 
       <nav>
         <section className='nav-buttons'>
+        <Link
+          to="about"
+          spy={true}
+          smooth={true}
+          duration={500}
+        >
           <button class="about-button">About</button>
+        </Link>
+        <Link
+          to="contact"
+          spy={true}
+          smooth={true}
+          duration={500}
+        >
           <button class="my-work-button">My Work</button>
+        </Link>
+        <Link
+          to="contact"
+          spy={true}
+          smooth={true}
+          duration={500}
+        >
           <button class="contact-button">Contact</button>
+        </Link>
         </section>
       </nav>
 
-      <div className='main-container'>
+      <div id='about' className='main-container'>
         <About/>
+        <Contact/>
       </div>
       
 
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,43 @@
+import "./contact.css";
+
+
+
+export default function Contact(){
+    return(
+        <div className="contact-container" id="contact">
+                <section>
+                    <h1>CONTACT LINKS</h1>
+                    <div className="one-link">
+                        <div>
+                            <img 
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png" 
+                            alt="linkedin logo"
+                            />
+                            <label>LinkedIn:</label>
+                        </div>
+                        <a href="https://www.linkedin.com/in/nurettin-demirhan">https://www.linkedin.com/in/nurettin-demirhan</a>
+                    </div>
+                    <div className="one-link">
+                        <div>
+                            <img 
+                            src="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png" 
+                            alt="linkedin logo"
+                            />
+                            <label>Github:</label>
+                        </div>
+                        <a href="https://www.github.com/NurettinDemirhan">https://www.github.com/NurettinDemirhan</a>
+                    </div>
+                    <div className="one-link">
+                        <div>
+                            <img 
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Gmail_Icon_%282013-2020%29.svg/2560px-Gmail_Icon_%282013-2020%29.svg.png" 
+                            alt="linkedin logo"
+                            />
+                            <label>Email:</label>
+                        </div>
+                        <a href="mailto:nurettindemirhan1998@gmail.com">nurettindemirhan1998@gmail.com</a>
+                    </div>
+                </section>
+        </div>
+    )
+}
